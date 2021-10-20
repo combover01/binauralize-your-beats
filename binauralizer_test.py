eb290dec6bd573b7285ad718c522361c6cde5cdb
talexander72 <78112431+talexander72@users.noreply.github.com>
2021-10-20 10:42:27 -0500

    Update binauralizer_test.py

--- binauralizer_test.py
+++ binauralizer_test.py
@@ -6,17 +6,17 @@ import numpy as np
 import scipy as sp
 import scipy.signal
 
-!spleeter separate -p spleeter:4stems -o output/ homage.wav       #### change 'homage.wav' to filename of choice
+!spleeter separate -p spleeter:4stems -o output/ homage.wav       #### SOURCE SEPARATION: change 'homage.wav' to filename of choice
 
 [stem,sr] = librosa.load('/Users/tuckeralexander/Desktop/spleeter/output/homage/bass.wav')       #### change path
 [original,sr] = librosa.load('/Users/tuckeralexander/Desktop/spleeter/homage.wav')                #### change path
 
 def nextpow2(x):
-    return int(np.ceil(np.log2(np.abs(x))))
+    return int(np.ceil(np.log2(np.abs(x))))            #### zero padding
 
 def freq_shift(x, f_shift, dt):
     N_orig = len(x)
-    N_padded = 2**nextpow2(N_orig)
+    N_padded = 2**nextpow2(N_orig)        #### zero padding
     t = np.arange(0, N_padded)
     return (scipy.signal.hilbert(np.hstack((x, np.zeros(N_padded-N_orig, x.dtype))))*np.exp(2j*np.pi*f_shift*dt*t))[:N_orig].real
 
@@ -27,9 +27,9 @@ t = np.arange(0, T, dt)
 N = len(t)
 
 
-shift = freq_shift(stem,10.0,dt) #stress relief beats
+shift = freq_shift(stem,10.0,dt)      #### stress relief frequency band
 gain = 1.5
-shift = shift * gain
+shift = shift * gain                  #### very basic and rudimentary gain control, will be improved soon
 
 test = original + shift
 write("test.wav",sr,test)
