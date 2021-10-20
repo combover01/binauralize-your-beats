37bd3a3a49b3a9c3e3c3380a7e79ac271a47b887
talexander72 <78112431+talexander72@users.noreply.github.com>
2021-10-20 11:09:59 -0500

    Update binauralizer_test.py

--- binauralizer_test.py
+++ binauralizer_test.py
@@ -8,15 +8,15 @@ import scipy.signal
 
 !spleeter separate -p spleeter:4stems -o output/ homage.wav       #### SOURCE SEPARATION: change 'homage.wav' to filename of choice
 
-[stem,sr] = librosa.load('/Users/tuckeralexander/Desktop/spleeter/output/homage/bass.wav')       #### change path
+[stem,sr] = librosa.load('/Users/tuckeralexander/Desktop/spleeter/output/homage/bass.wav')        #### change path
 [original,sr] = librosa.load('/Users/tuckeralexander/Desktop/spleeter/homage.wav')                #### change path
 
 def nextpow2(x):
-    return int(np.ceil(np.log2(np.abs(x))))            #### zero padding
+    return int(np.ceil(np.log2(np.abs(x))))            #### for zero padding inside freq_shift function
 
-def freq_shift(x, f_shift, dt):
+def freq_shift(x, f_shift, dt):                        #### frequency shifting function
     N_orig = len(x)
-    N_padded = 2**nextpow2(N_orig)        #### zero padding
+    N_padded = 2**nextpow2(N_orig)                     #### zero padding
     t = np.arange(0, N_padded)
     return (scipy.signal.hilbert(np.hstack((x, np.zeros(N_padded-N_orig, x.dtype))))*np.exp(2j*np.pi*f_shift*dt*t))[:N_orig].real
 
@@ -27,9 +27,14 @@ t = np.arange(0, T, dt)
 N = len(t)
 
 
-shift = freq_shift(stem,10.0,dt)      #### stress relief frequency band
-gain = 1.5
-shift = shift * gain                  #### very basic and rudimentary gain control, will be improved soon
+def binauralizer(band,gain):           #### creates stereo signal, each channel mixed with original audio for sound quality
+    leftear = stem * gain              #### work in progress
+    leftear = original + leftear
+    shift = freq_shift(stem,band,dt)
+    rightear = shift * gain
+    rightear = original + rightear
+    return (leftear,rightear)
 
-test = original + shift
+#testcase   
+# [leftear,rightear] = binauralizer(10.0,0.8)
 write("test.wav",sr,test)
